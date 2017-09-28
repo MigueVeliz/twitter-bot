4553f63958adf8f59b472ea12e71a4d3e551ed17
Miguel Angel Veliz <MigueVeliz@users.noreply.github.com>
2017-09-28 11:57:01 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
 
-	consumer_key: 'mBqbGziPAX49IMeR4W5ybYHEE',
-	consumer_secret: 'cgvBYuWPMWNjzzhhIJJxkSNI3o0PpgDt7qIBSe4ED3p35YNIRD',
-	access_token: '903498499777413121-a9zD6ZHS81bSem3Niy1SpLnbAB0D7HK',
-	access_token_secret: '0IVbq7UHs7mCJMk9Qk4GTfYyCFEs1zNHO1Jc0NIOWnKuW'
+	consumer_key: '',
+	consumer_secret: '',
+	access_token: '',
+	access_token_secret: ''
 
 
 }
